3ed90c47811e70f9e17145d8100f26fc77c74566
marijkevanhell <jukevanhell@gmail.com>
2023-03-29 17:47:02 -0700

    add second route

--- express_server.js
+++ express_server.js
@@ -27,3 +27,8 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
