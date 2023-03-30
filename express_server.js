f793dabb9b78ee754c44c2c3f2108c76a5b121cb
marijkevanhell <jukevanhell@gmail.com>
2023-03-29 19:08:28 -0700

    organized better with notes

--- express_server.js
+++ express_server.js
@@ -1,23 +1,22 @@
-function generateRandomString() {}
-
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 6)
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//middleware
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
+//routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -27,21 +26,24 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+
+//server listening
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
