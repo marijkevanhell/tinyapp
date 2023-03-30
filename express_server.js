8e03693996438c8155900471c35652f265606aa5
marijkevanhell <jukevanhell@gmail.com>
2023-03-29 18:34:18 -0700

    shorten part 1

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+function generateRandomString() {}
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -8,6 +10,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -28,7 +32,16 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
